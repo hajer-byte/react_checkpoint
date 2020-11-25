bc3316c4a4318f51361fb4e288ad236393b132f1
HAJER <hajer.mejri@contentpepper.de>
2020-11-25 11:42:48 +0100

    first website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./App.css";
+import { MDBContainer, MDBRow, MDBCol, MDBInput } from "mdbreact";
+import Button from "react-bootstrap/Button";
+function Form() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MDBContainer>
+      <MDBRow>
+        <MDBCol md="6" className="image"></MDBCol>
+        <MDBCol md="6">
+          <form>
+            <p className="text_style h5 text-center mb-4">Login</p>
+            <div className="grey-text">
+              <MDBInput
+                label="Your name"
+                icon="user"
+                group
+                type="text"
+                validate
+                error="wrong"
+                success="right"
+              />
+              <MDBInput
+                label="Your email"
+                icon="envelope"
+                group
+                type="email"
+                validate
+                error="wrong"
+                success="right"
+              />
+            </div>
+            <div className="text-center">
+              <Button variant="danger">Login</Button>
+            </div>
+          </form>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
   );
 }
 
-export default App;
+export default Form;
